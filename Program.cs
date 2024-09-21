a91c5e3250d66907f4326210cb2eefd92cbca6ed
unknown <fabriciosipauba2@gmail.com>
2024-09-21 16:44:02 -0300

    Adiçao do Cronometro

--- Program.cs
+++ Program.cs
@@ -3,14 +3,15 @@ namespace CourseApps
 {
 
 
-internal class Program
-{
-    public static void Main(string[] args)
+    internal class Program
     {
+        public static void Main(string[] args)
+        {
+            
+            //Calculadora.Menu();
+            StopWatch.Menu();
 
-        Calculadora.Menu();
-
+        }
     }
-}
 
 }
